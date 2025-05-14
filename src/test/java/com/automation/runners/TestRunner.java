2b013c100131f95dc8c3df4688066c96200786c9
sumithtest1997 <162404732+sumithtest1997@users.noreply.github.com>
2025-05-14 19:01:23 -0400

    Extent Reports Test

--- src/test/java/com/automation/runners/TestRunner.java
+++ src/test/java/com/automation/runners/TestRunner.java
@@ -2,6 +2,7 @@ package com.automation.runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
     features = "src/test/resources/features",
@@ -9,8 +10,13 @@ import io.cucumber.testng.CucumberOptions;
     plugin = {
         "pretty",
         "html:target/cucumber-reports/cucumber-pretty.html",
-        "json:target/cucumber-reports/CucumberTestReport.json"
-    }
+        "json:target/cucumber-reports/CucumberTestReport.json",
+        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+    },
+    monochrome = true,
+    dryRun = false,
+    publish = true
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
+
 } 
